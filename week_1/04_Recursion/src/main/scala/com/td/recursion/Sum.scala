833cc7e34a3e0b933ed6b443c902e16f7b111890
Taeith <nils.pernet@gmail.com>
2020-11-25 15:53:23 +0100

    week1/recursion

--- week_1/04_Recursion/src/main/scala/com/td/recursion/Sum.scala
+++ week_1/04_Recursion/src/main/scala/com/td/recursion/Sum.scala
@@ -4,8 +4,13 @@ object Sum {
 
   def closedForm(n: Long): Long = (n * (n + 1)) / 2;
 
-  def recursive(n: Long): Long = ???
+  def recursive(n: Long): Long = {
+  	n match {
+  		case x if x < 1 => 0
+  		case _ => recursive(n - 1) + n
+  	}
+  }
 
-  def tailrecursive(n: Long): Long = ???
+  def tailrecursive(n: Long): Long = if (n == 0) 0 else n + tailrecursive(n - 1)
 
 }
