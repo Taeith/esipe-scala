3aadd5c4e962b4cb3fb9de517316ab5385096e90
Taeith <nils.pernet@gmail.com>
2020-11-30 15:42:53 +0100

    week3/implicits

--- week_3/08_Implicits/src/main/scala/com/td/implicits/OptionHelpers.scala
+++ week_3/08_Implicits/src/main/scala/com/td/implicits/OptionHelpers.scala
@@ -3,9 +3,26 @@ package com.td.implicits
 object OptionHelpers {
 
   // 1: Implement this function
-  def naiveAdd(option1: Option[Int], option2: Option[Int]): Option[Int] = ???
+  def naiveAdd(option1: Option[Int], option2: Option[Int]): Option[Int] = {
+  	option1 match {
+        case Some(value) => Some(value + option2.getOrElse(0))
+        case None => None
+    }
+  }
 
   // 2: Implement an extension method that adds two Option[Int]
-
   // 3: Implement a solution to add option and int
+
+	implicit class CustomOption(val option1: Option[Int]) {
+	    def plus(option2: Option[Int]): Option[Int] = {
+	        naiveAdd(option1, option2)
+	    }
+	    def +(toAdd: Int): Option[Int] = {
+	    	option1 match {
+		        case Some(value) => Some(value + toAdd)
+		        case None => None
+		    }
+	    }
+	}
+	
 }
